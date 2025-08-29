c9bf509bc78c5105fa1457cfc9f015f903f52f3b
Chris <chrisonchRI5677@gmail.com>
2025-08-29 23:37:03 +0300

    circular linked list

--- index.js
+++ index.js
@@ -48,3 +48,76 @@ display(){
         current=current.next;
     } 
 }
+//circular linked list
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null; // Initially points to null
+  }
+}
+
+class CircularLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.size = 0;
+  }
+
+  // Method to add a new node to the end of the list
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      // If the list is empty, the new node is both head and tail
+      this.head = newNode;
+      this.tail = newNode;
+      newNode.next = this.head; // Points to itself to form a circle
+    } else {
+      // If the list is not empty, add to the end and update pointers
+      this.tail.next = newNode;
+      newNode.next = this.head; // New node points back to the head
+      this.tail = newNode; // Update tail to the new node
+    }
+    this.size++;
+  }
+
+  // Method to add a new node to the beginning of the list
+  prepend(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      newNode.next = this.head;
+    } else {
+      newNode.next = this.head; // New node points to the current head
+      this.head = newNode; // Update head to the new node
+      this.tail.next = this.head; // Tail's next still points to the new head
+    }
+    this.size++;
+  }
+
+  // Method to traverse and print the circular linked list
+  printList() {
+    if (!this.head) {
+      console.log("List is empty.");
+      return;
+    }
+    let current = this.head;
+    let result = "";
+    do {
+      result += `${current.value} -> `;
+      current = current.next;
+    } while (current !== this.head);
+    result += "(back to head)"; // Indicate the circular nature
+    console.log(result);
+  }
+}
+
+// Example usage:
+const circularList = new CircularLinkedList();
+circularList.append(10);
+circularList.append(20);
+circularList.prepend(5);
+circularList.append(30);
+
+circularList.printList(); // Output: 5 -> 10 -> 20 -> 30 -> (back to head)
+}
